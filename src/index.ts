6ba6c162f5e14e4d9c3b6912faa1bce7296f64ec
floralatin <dragondao@hotmail.com>
2022-06-20 18:39:31 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -17,36 +17,8 @@ type Options = {
 
 async function main() {
   const token = core.getInput('github-token', {required: true})
-  const debug = core.getInput('debug')
-  const userAgent = core.getInput('user-agent')
-  const previews = core.getInput('previews')
-
   const opts: Options = {}
-  if (debug === 'true') opts.log = console
-  if (userAgent != null) opts.userAgent = userAgent
-  if (previews != null) opts.previews = previews.split(',')
-
   const github = getOctokit(token, opts)
-  let actions = core.getInput('actions');
-  // Using property/value shorthand on `require` (e.g. `{require}`) causes compilation errors.
-  // const result = await callAsyncFunction(
-  //   {
-  //     require: wrapRequire,
-  //     __original_require__: __non_webpack_require__,
-  //     github,
-  //     context,
-  //     core,
-  //     exec,
-  //     glob,
-  //     io
-  //   },
-  //   `return await github.rest.actions.createWorkflowDispatch({
-  //     owner: 'echoiitime-inc',
-  //     repo: 'hello-world-javascript-action',
-  //     workflow_id: 'node_test.yml',
-  //     ref: 'master'
-  //   })`
-  // ) 
   const result = await github.rest.actions.createWorkflowDispatch({
     owner: 'echoiitime-inc',
     repo: 'hello-world-javascript-action',
